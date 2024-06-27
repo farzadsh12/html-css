2d857ec0e27785d7b94acb0b4d3e93939bfff225
Farzad <97731951+farzadsh11@users.noreply.github.com>
2024-06-27 19:03:41 +0330

    Update app.js

--- app.js
+++ app.js
@@ -14,3 +14,52 @@ console.log(20 + 10);
 console.log("Farzad " + "Sh");
 console.log("I have to go, then come back");
 console.log(100 * 2);
+
+
+const book = "programming";
+const book2 = "English";
+const text = book + " and " + book2 + ".";
+console.log(text); 
+
+
+let number = 5;
+number = 10;
+console.log(number);
+
+let camelCase = "farzad";
+console.log(camelCase);
+
+
+const YOUR_BIRTHDAY = "2023";
+console.log(YOUR_BIRTHDAY);
+
+
+console.log(true);
+
+const display = false;
+console.log(display);
+
+
+const display1 = !false;
+console.log(display1);
+
+
+const num1 = 10;
+const num2 = 20;
+console.log(num1 === num2);
+
+console.log(display === display1);
+
+console.log(num1 !== num2);
+
+const num3 = 10;
+
+console.log(num3 !== num1);
+
+
+
+
+// lesson 02
+
+
+
